206052e0a1e964d224ef403339440d00a0da82b6
Tiberiu <lordiulian@gmail.com>
2021-10-01 21:07:06 +0200

    frontend prepared

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,8 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
 
-// any CSS you import will output into a single css file (app.css in this case)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Wikelian from './components/Wikelian';
+
 import './styles/app.css';
+
+ReactDOM.render(<Wikelian />, document.getElementById('root'));
