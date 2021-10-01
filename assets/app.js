1a6908a9c10381d4fea4dce3aaca7d675dd4cdc4
Tiberiu <lordiulian@gmail.com>
2021-10-01 21:53:35 +0200

    better withour react

--- assets/app.js
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Wikelian from './components/Wikelian';
-
-import './styles/app.css';
-
-ReactDOM.render(<Wikelian />, document.getElementById('root'));
